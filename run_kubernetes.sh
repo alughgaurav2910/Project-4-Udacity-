e09c275bf0d068fd44c80d183c5d8d43461ac0cb
alughgaurav2910 <gaurav.mca11@gmail.com>
2020-09-14 01:34:14 +0530

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,7 +8,7 @@ dockerpath=gauravalugh/m1-microservice:v2
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run --image $dockerpath ml-app --port=8000
+kubectl run --image $dockerpath m1-app --port=8000
 
 # Step 3:
 # List kubernetes pods
@@ -16,4 +16,4 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward deployment/ml-app 8000:80
+kubectl port-forward deployment/m1-app 8000:80
